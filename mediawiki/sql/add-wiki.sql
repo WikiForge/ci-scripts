1ec52382d2d63a9fcc3ae293e9d8437a992e516c
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-23 12:56:40 -0600

    Try using raw sql

---
+++ mediawiki/sql/add-wiki.sql
@@ -0,0 +1,29 @@
+INSERT INTO cw_wikis (
+    wiki_dbname,
+    wiki_dbcluster,
+    wiki_sitename,
+    wiki_language,
+    wiki_private,
+    wiki_creation,
+    wiki_category,
+    wiki_closed,
+    wiki_deleted,
+    wiki_locked,
+    wiki_inactive,
+    wiki_inactive_exempt,
+    wiki_url
+) VALUES (
+    'wikidb',
+    'c1',
+    'TestWiki',
+    'en',
+    0,
+    UNIX_TIMESTAMP(),
+    'uncategorized',
+    0,
+    0,
+    0,
+    0,
+    0,
+    'http://127.0.0.1:9412'
+)
